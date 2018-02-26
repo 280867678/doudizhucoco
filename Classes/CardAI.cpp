3f77c27f6a9efaac0e970369ab0a4954664f7d2a
Ray1024 <guojianlei010@163.com>
2018-02-26 16:15:49 +0800

    initial commit

---
+++ Classes/CardAI.cpp
@@ -0,0 +1,20 @@
+#include "CardAI.h"
+
+USING_NS_CC;
+
+CARDTYPE CardAI::JudgeCardsType(std::vector<CardInfo>& sendCard)
+{
+	// 牌小于5张：
+	if (sendCard.size()<5)
+	{
+
+	}
+	// 牌大于5张：
+	else
+	{
+
+	}
+
+
+	return CARDTYPE::ERRORCARDS;
+}
---
+++ Classes/CardManager.h
@@ -0,0 +1,31 @@
+#ifndef __CARD_MANAGER_H__
+#define __CARD_MANAGER_H__
+
+#include "cocos2d.h"
+#include "CardExhibitionZone.h"
+
+class CardManager : public cocos2d::Sprite
+{
+
+public:
+
+	static CardManager* create();
+
+    virtual bool init();
+
+	bool dealer(CardInfo info);
+
+	void updateCards();
+
+	void chuPai();
+
+	virtual void clearCards();
+
+protected:
+
+	CardExhibitionZone* _exhibitionZone;
+
+	std::vector<CardInfo> _cardIndex;
+};
+
+#endif // __CARD_MANAGER_H__
